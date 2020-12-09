b04bc5d112a788bc0f0ab243eceb4b483ea7296a
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-12-08 23:32:21 -0800

    Initialize repository

---
+++ lib/marcjaramillo_palindrome.rb
@@ -0,0 +1,6 @@
+require "marcjaramillo_palindrome/version"
+
+module MarcjaramilloPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/marcjaramillo_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class MarcjaramilloPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MarcjaramilloPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
